163baf408930e4fefb40b6e17313976113e39020
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-04-28 01:53:30 +0300

    rclone data download

--- setup.sh
+++ setup.sh
@@ -1,11 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ ! -e attributes.csv.gz ]]
-then
-    gsutil cp gs://di-datasets/home-depot-search-relevance/attributes.csv.gz .
-fi
-
-if [[ ! -e product_descriptions.csv.gz ]]
-then
-    gsutil cp gs://di-datasets/home-depot-search-relevance/product_descriptions.csv.gz .
-fi
+rclone copy -P gcs:di-datasets/home-depot-search-relevance data
