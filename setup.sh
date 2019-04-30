28fbb42c59e788d5f1dff86ffb9b14f1f7f2b70e
Dmitri Babaev <dmitri.babaev@gmail.com>
2019-05-01 01:47:35 +0300

    remote storage setup doc

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
+# use the following command to automatically setup the remote storage: rclone config create gcs "google cloud storage"
 rclone copy -P gcs:di-datasets/home-depot-search-relevance data
