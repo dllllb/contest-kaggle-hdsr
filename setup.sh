46670667e94ed43056e5160225a0c4ac9a81e0d8
Dmitri Babaev <dmitri.babaev@gmail.com>
2020-05-26 21:46:10 +0300

    single file dataset

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
 # use the following command to automatically setup the remote storage: rclone config create gcs "google cloud storage"
-rclone copy -P gcs:di-datasets/home-depot-search-relevance data
+rclone copy -P gcs:di-datasets/home-depot-search-relevance.zip data
+cd data
+unzip home-depot-search-relevance.zip
