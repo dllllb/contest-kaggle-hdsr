02653831301944b2530d5f53feca089ee5ee4681
Dmitri Babaev <dmitri.babaev@gmail.com>
2021-02-28 11:53:24 +0300

    minor

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# use the following command to automatically setup the remote storage: rclone config create gcs "google cloud storage"
+# use the following command to automatically setup the remote storage: `rclone config create gcs "google cloud storage"`
 rclone copy -P gcs:di-datasets/home-depot-search-relevance.zip data
 cd data
 unzip home-depot-search-relevance.zip
